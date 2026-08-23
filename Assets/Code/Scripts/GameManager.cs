8bc5607f2f97f029ad6c2998ff877f3ca9f4b9ae
Evrenefeb <141721390+Evrenefeb@users.noreply.github.com>
2026-08-24 01:46:49 +0300

    Pre-PlayerButton

--- Assets/Code/Scripts/GameManager.cs
+++ Assets/Code/Scripts/GameManager.cs
@@ -3,22 +3,31 @@ using Sirenix.OdinInspector;
 using System;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
 
 
 
-    [SerializeField] private GameManagerSettings r_Settings;
-    [SerializeField] [ReadOnly] private TrustSequence m_CurrentTrustSequence;
+    [SerializeField] private GameManagerSettings r_GameManagerSettings;
+    [SerializeField] private TrustSequenceSettings r_TrustSequenceSettings;
+
+
+
+    [SerializeField] private PlayerButton r_PlayerButton;
+
+
+
+
+    [SerializeField][ReadOnly] private TrustSequence m_CurrentTrustSequence;
+
+
+
 
     private CountdownTimer m_TrustSequenceTimer;
 
 
 
-
     public CountdownTimer TrustSequenceTimer => m_TrustSequenceTimer;
     public TrustSequence CurrentTrustSequence => m_CurrentTrustSequence;
-
 
 
 
@@ -35,54 +44,80 @@ public class GameManager : MonoBehaviour
     #region Initialization
 
     private void Initialize() {
-        m_TrustSequenceTimer = new CountdownTimer(r_Settings.InitTrustSequenceTimerValue);
+        m_TrustSequenceTimer = new CountdownTimer(r_GameManagerSettings.InitTrustSequenceTimerValue);
 
         m_TrustSequenceTimer.OnTimerStart += OnGameTrustSequenceTimerStart;
         m_TrustSequenceTimer.OnTimerStop += OnGameTrustSequenceTimerStop;
 
-        // Start New TrustSequence
-        m_CurrentTrustSequence = new TrustSequence();
+        
     }
 
     #endregion
 
+    #region Timer Events
+
     public void OnGameTrustSequenceTimerStart() {
+
+        m_CurrentTrustSequence ??= new TrustSequence(r_TrustSequenceSettings);
 
         m_TrustSequenceTimer.Reset();
         m_TrustSequenceTimer.Start();
     }
 
     public void OnGameTrustSequenceTimerStop() {
-        if(m_TrustSequenceTimer.IsRunning) m_TrustSequenceTimer.Stop();        
+        if (m_TrustSequenceTimer.IsRunning) m_TrustSequenceTimer.Stop();
 
-        CurrentTrustSequence.CallNextSequnce();
+        CurrentTrustSequence.CallNextSequence();
+    }
+
+    #endregion
+
+    public void Player_OnDown() {
+        Debug.Log("Player_OnDown");
+    }
+
+    public void Player_OnClick() {
+        Debug.Log("Player_OnPress");
     }
 
 
-    
 
 
 }
 
 [Serializable]
-public class TrustSequence {
+public class TrustSequence : ITrustSequenceLike {
 
-    // TODO: Change props from hard coded values
+    private int m_PotIndex;
+    private float m_CurrentPot;
 
-    public int PotIndex;
-    public float CurrentPot;
+    public int PotIndex => m_PotIndex;
+    public float CurrentPot => m_CurrentPot;
+
+    public TrustSequence(TrustSequenceSettings sequenceSettings) {
+        CallFirstSequence(sequenceSettings.CurrentPot);
+    }
 
     public event Action OnNextSequenceCalled;
 
-    public TrustSequence() {
-        PotIndex = 0;
-        CurrentPot = 10000;
-    }
 
-    public void CallNextSequnce() {
-        CurrentPot *= 2;
-        PotIndex += 1;
+    public void CallNextSequence() {
+
+        m_PotIndex += 1;
+        m_CurrentPot *= 2;
 
         OnNextSequenceCalled?.Invoke();
     }
+
+    private void CallFirstSequence(float currentPot) {
+        m_PotIndex = 0;
+        m_CurrentPot = currentPot;
+
+        OnNextSequenceCalled?.Invoke();
+    }
+}
+
+public interface ITrustSequenceLike {
+    public int PotIndex { get; }
+    public float CurrentPot { get; }
 }
--- Assets/Code/Scripts/InGameUIManager.cs
+++ Assets/Code/Scripts/InGameUIManager.cs
@@ -8,8 +8,16 @@ public class InGameUIManager : MonoBehaviour {
     [SerializeField] private GameManager r_GameManager;
 
     [SerializeField] private TMP_Text txt_TimerTime;
+    [SerializeField] private TMP_Text txt_PotIndex;
+    [SerializeField] private TMP_Text txt_CurrentPot;
 
     #region Unity Methods
+
+    private void Start() {
+        Initialize();
+    }
+
+
 
     private void Update() {
         UpdateTrustSequenceTimer();
@@ -17,9 +25,22 @@ public class InGameUIManager : MonoBehaviour {
 
     #endregion
 
-    private void UpdateTrustSequenceTimer() {
-        txt_TimerTime.text = r_GameManager.TrustSequenceTimer.CurrentTime.ToString("F2");
+    private void Initialize() {
+        r_GameManager.CurrentTrustSequence.OnNextSequenceCalled += UpdatePotIndex;
+        r_GameManager.CurrentTrustSequence.OnNextSequenceCalled += UpdateCurrentPot;
     }
+
+
+
+    #region UI Update Methods
+
+    private void UpdatePotIndex() => txt_PotIndex.text = r_GameManager.CurrentTrustSequence.PotIndex.ToString();
+    private void UpdateCurrentPot() => txt_CurrentPot.text = r_GameManager.CurrentTrustSequence.CurrentPot.ToString("F2");
+    private void UpdateTrustSequenceTimer() => txt_TimerTime.text = (r_GameManager.TrustSequenceTimer.CurrentTime < 0) ? "00.000" : r_GameManager.TrustSequenceTimer.CurrentTime.ToString("F3");
+
+    #endregion
+
+
 
 
 }
