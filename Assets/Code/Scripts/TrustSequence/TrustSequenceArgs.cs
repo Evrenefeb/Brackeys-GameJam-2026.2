832599af4833337542217a6e9a9144c720867162
Evrenefeb <141721390+Evrenefeb@users.noreply.github.com>
2026-08-24 16:07:47 +0300

    Refactor

---
+++ Assets/Code/Scripts/TrustSequence/TrustSequenceArgs.cs
@@ -0,0 +1,8 @@
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TrustSequenceArgs", menuName = "TrustSequence/Args", order = 0)]
+[Serializable]
+public class TrustSequenceArgs : ScriptableObject {
+    public string p_SequenceName;
+}
---
+++ Assets/Code/Scripts/TrustSequence/TrustSequenceManager.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+public class TrustSequenceManager : MonoBehaviour
+{
+    [SerializeField] private TrustSequenceType p_SequenceType = TrustSequenceType.CAMPAIGN;
+    [SerializeField] private TrustSequenceArgs p_SequenceArgs;
+
+
+    private void OnEnable() {
+        StartTrustSequence();
+    }
+
+    public void StartTrustSequence() {
+        Debug.Log("Starting Trust Sequence: " + p_SequenceArgs.p_SequenceName);
+    }
+}
